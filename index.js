855e5dd15f204cab1dd997aed82993bc225e867d
Faith <loshyfaith@gmail.com>
2024-03-28 01:24:24 +0300

    my array

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats=["Milo","Otis","Garfield"];
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+    const Blackcats=[...cats, 'Broom'];
+    return Blackcats;
+}
+function prependCat() {
+    const Blackcats=['Arnold', ...cats];
+    return Blackcats;
+}
+function removeLastCat() {
+    return cats.slice(0,2);
+}
+function removeFirstCat() {
+    return cats.slice(1,3);
+}
+
+
